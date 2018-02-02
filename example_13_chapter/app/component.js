e8e846368074f0a36d614eaae0973b5b84e92c1d
Yauheni Svirydzenka <partagas@mail.ru>
2018-02-02 22:59:53 +0300

    Chapter 13 add Chapter 14 add

---
+++ example_13_chapter/app/component.js
@@ -0,0 +1,63 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+var core_1 = require("@angular/core");
+var repository_model_1 = require("./repository.model");
+var ProductComponent = (function () {
+    function ProductComponent() {
+        this.model = new repository_model_1.Model();
+        this.fontSizeWithUnits = "30px";
+        this.fontSizeWithoutUnits = "30";
+        this.targetName = "Kayak";
+    }
+    /*getClasses(): string {
+        return this.model.getProducts().length == 5 ? "bg-success" : "bg-warning";
+    }*/
+    ProductComponent.prototype.getClasses = function (key) {
+        var product = this.model.getProduct(key);
+        return "p-a-1 " + (product.price < 50 ? "bg-info" : "bg-warning");
+    };
+    ProductComponent.prototype.getClassMap = function (key) {
+        var product = this.model.getProduct(key);
+        return {
+            "text-xs-center bg-danger": product.name == "Kayak",
+            "bg-info": product.price < 50
+        };
+    };
+    ProductComponent.prototype.getStyles = function (key) {
+        var product = this.model.getProduct(key);
+        return {
+            fontSize: "30px",
+            "margin.px": 100,
+            color: product.price > 50 ? "red" : "green"
+        };
+    };
+    ProductComponent.prototype.getProductByPosition = function (position) {
+        return this.model.getProducts()[position];
+    };
+    ProductComponent.prototype.getProduct = function (key) {
+        return this.model.getProduct(key);
+    };
+    ProductComponent.prototype.getProducts = function () {
+        return this.model.getProducts();
+    };
+    ProductComponent.prototype.getProductCount = function () {
+        return this.getProducts().length;
+    };
+    ProductComponent = __decorate([
+        core_1.Component({
+            selector: "app",
+            templateUrl: "app/template.html"
+        }), 
+        __metadata('design:paramtypes', [])
+    ], ProductComponent);
+    return ProductComponent;
+}());
+exports.ProductComponent = ProductComponent;
---
+++ example_13_chapter/app/component.ts
@@ -0,0 +1,58 @@
+﻿import { Component } from "@angular/core";
+import { Model } from "./repository.model";
+import { Product } from "./product.model";
+
+@Component({
+    selector: "app",
+    templateUrl: "app/template.html"
+})
+export class ProductComponent {
+    model: Model = new Model();
+
+    /*getClasses(): string {
+        return this.model.getProducts().length == 5 ? "bg-success" : "bg-warning";
+    }*/
+
+    getClasses(key: number): string {
+        let product = this.model.getProduct(key);
+        return "p-a-1 " + (product.price < 50 ? "bg-info" : "bg-warning");
+    }
+
+     getClassMap(key: number): Object {
+        let product = this.model.getProduct(key);
+        return {
+            "text-xs-center bg-danger": product.name == "Kayak",
+            "bg-info": product.price < 50
+        };
+    }
+
+    fontSizeWithUnits: string = "30px";
+    fontSizeWithoutUnits: string= "30";
+
+    getStyles(key: number) {
+        let product = this.model.getProduct(key);
+        return {
+            fontSize: "30px",
+            "margin.px": 100,
+            color: product.price > 50 ? "red" : "green"
+        };
+    }
+
+    getProductByPosition(position: number): Product {
+        return this.model.getProducts()[position];
+    }
+
+    getProduct(key: number): Product {
+        return this.model.getProduct(key);
+    }
+
+    getProducts(): Product[] {
+        return this.model.getProducts();
+    }
+
+    getProductCount(): number {
+        return this.getProducts().length;
+    }
+
+    targetName: string = "Kayak";
+}
