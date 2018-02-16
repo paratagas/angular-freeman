7e9460a40ac5c9a41cda6767f92e5667215a0ac4
Yauheni Svirydzenka <partagas@mail.ru>
2018-02-16 20:57:25 +0300

    Add chapters 16-21

---
+++ example_18_chapter/app/component.ts
@@ -0,0 +1,18 @@
+﻿import { ApplicationRef, Component } from "@angular/core";
+import { Model } from "./repository.model";
+import { Product } from "./product.model";
+import { ProductFormGroup } from "./form.model";
+
+@Component({
+    selector: "app",
+    templateUrl: "app/template.html",
+    //styles: ["div { border: 2px black solid;  font-style:italic }"]
+    //styles: ["/deep/ div { border: 2px black solid;  font-style:italic }"]
+})
+
+export class ProductComponent {
+    model: Model = new Model();
+    addProduct(p: Product) {
+        this.model.saveProduct(p);
+    }
+}
